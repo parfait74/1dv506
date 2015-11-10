d9c73fb68bf964d7416e0e820d2f0920e25c1d0c
Fredrik Sandberg <fredrik@advancera.se>
2015-11-10 21:55:38 +0100

    added 14

---
+++ src/fs222mh_lab1/TelefonNummer.java
@@ -0,0 +1,23 @@
+package fs222mh_lab1;
+
+import java.util.Random;
+
+public class TelefonNummer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int r1 = rand.nextInt(10);
+		int r2 = rand.nextInt(10);
+		int r3 = rand.nextInt(10);
+		int l1 = rand.nextInt(9)+1;
+		int l2 = rand.nextInt(10);
+		int l3 = rand.nextInt(10);
+		int l4 = rand.nextInt(10);
+		int l5 = rand.nextInt(10);
+
+		System.out.println("0"+r1+r2+r3+"-"+l1+l2+l3+l4+l5);
+
+	}
+
+}
