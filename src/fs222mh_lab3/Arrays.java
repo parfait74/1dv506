5cbac033f7d753973530db7457e4059f709d66d8
Fredrik Sandberg <fredrik@advancera.se>
2015-12-16 18:31:56 +0100

    lab3 ex1

--- src/fs222mh_lab3/Arrays.java
+++ src/fs222mh_lab3/Arrays.java
@@ -3,10 +3,10 @@ package fs222mh_lab3;
 public class Arrays {
 
 	public static void main(String[] args) {
-		int[] data = {1,2,3,4,5}; //Array to be used in examples
+		int[] data = {1,2,3,6,4,9,5,2}; //Array to be used in examples
 		
 		int total = sum(data);
-		System.out.print(total + "\n");
+		System.out.print("Summa: " + total + "\n");
 		
 		String str = Arrays.toString(data);
 		System.out.print("data = " + str + "\n");
@@ -15,7 +15,13 @@ public class Arrays {
 		System.out.print(toString(rArray) + "\n");
 		
 		int n = 4;
-		System.out.print(hasN(data,n));
+		System.out.print(hasN(data,n) + "\n");
+		
+		int old = 2;
+		int nw = 11;
+		replaceAll(data, old, nw);
+		
+		System.out.print(toString(sort(data)) + "\n" );
 
 
 	}
@@ -64,9 +70,50 @@ public class Arrays {
 	}
 	
 	private static void replaceAll(int[] arr, int old, int nw) {
+		int[] nArr = new int[arr.length];
+		
+		for (int i = 0; i < arr.length; i += 1){
+			if ( arr[i] == old) {
+				nArr[i] = nw;
+			}
+			else {
+				nArr[i] = arr[i];
+			}
+		}
+		System.out.print(toString(nArr) + "\n");
 		
 	}
 	
+	private static int[] sort(int[] arr) {
+		int[] sortedArr = new int[arr.length];
+		
+		//Copy original copy to keep original
+		for (int i = 0; i < arr.length; i += 1) {
+			sortedArr[i] = arr[i];
+		}
+		
+		for (int i = 1; i < sortedArr.length; i += 1) {
+			if ( sortedArr[i] < sortedArr[i-1]) {
+				sortedArr[i] = sortedArr[i] + sortedArr[i-1];
+				sortedArr[i-1] = sortedArr[i] - sortedArr[i-1];
+				sortedArr[i] = sortedArr[i] - sortedArr[i-1];
+				i = 0;					// to loop through/back to beginning of array
+				
+			}
+		}
+		return sortedArr;
+	}
+	
+	private static boolean hasSubSequence(int[] arr, int[] sub) {
+		
+			for (int i = 0; i < arr.length; i += 1) {
+			if (arr.length - i > sub.length || arr[i] == sub[0]) {
+				
+				
+				}
+			}
+		
+	}
 	
 	
 }
